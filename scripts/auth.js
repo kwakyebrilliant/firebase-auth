8acb2647ce0394e7174a704a27bf15468db94c57
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-09-25 12:09:33 +0000

    updates

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,5 +2,11 @@
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
+    // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  console.log(email, password);
+
 })
