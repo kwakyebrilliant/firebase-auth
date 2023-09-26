dfc9f87ec9eee2d06301acd22817eb106e6137ad
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-09-26 15:35:18 +0000

    auth updates

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,7 +43,10 @@ signupForm.addEventListener('submit', (e) => {
 
   // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
+  }).then(() => {
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
