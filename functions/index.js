65e510bf61fb440bd92a5be7832c3629fc7d333a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-09-29 08:23:57 +0000

    functions updates

--- functions/index.js
+++ functions/index.js
@@ -5,4 +5,15 @@ admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
     //get user and custom claim (admin)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+          admin: true
+        })
+      }).then(() => {
+        return {
+          message: `Success! ${data.email} has been made an admin.`
+        }
+      }).catch(err => {
+        return err;
+      });
 });
